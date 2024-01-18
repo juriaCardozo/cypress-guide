335b0c9754d6ff61a832646f23c018da75981929
julia.cavalcante <ccardozojulia@gmail.com>
2024-01-18 11:23:28 -0300

    adjusting Hardcoded assertions

--- cypress/e2e/hardcoded-assertion/sample.cy.js
+++ cypress/e2e/hardcoded-assertion/sample.cy.js
@@ -1,3 +1,5 @@
+const { hits } = require('../../fixtures/stories')
+
 describe('Hardcoded assertion bad practice', () => {
   beforeEach(() => {
     cy.intercept(
@@ -10,18 +12,32 @@ describe('Hardcoded assertion bad practice', () => {
     cy.wait('@getStories')
   })
 
-  it('searches', () => {
-    cy.search('cypress.io')
-    cy.wait('@getStories')
+  // it('searches with cy.fixture', () => {
+  //   cy.fixture('stories').then(({hits}) => {
+  //     cy.search('cypress.io')
+  //     cy.wait('@getStories')
 
+
+  //     cy.get('.table-row')
+  //       .as('tableRows')
+  //       .should('have.length', hits.length)
+      
+  //     hits.forEach((hit, index) => {
+  //       cy.get('@tableRows')
+  //         .eq(index)
+  //         .should('contain', hit.title) 
+  //     })
+  //   })
+  // })
+
+  it('searches with javascript objects', () => {
     cy.get('.table-row')
       .as('tableRows')
-      .should('have.length', 2)
-    cy.get('@tableRows')
-      .eq(0)
-      .should('contain', 'Agile Testing')
-    cy.get('@tableRows')
-      .eq(1)
-      .should('contain', 'Clean Code')
+      .should('have.length', hits.length)
+    hits.forEach((hit, index) => {
+      cy.get('@tableRows')
+        .eq(index)
+        .should('contain', hit.title) 
+    })
   })
 })
